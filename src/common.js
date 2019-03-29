9fa373f81cb5cfd823a279ede708b89eca3fbff6
myj <2908037646@qq.com>
2019-03-29 11:54:10 +0800

    first commit

---
+++ src/common.js
@@ -0,0 +1,23 @@
+/**
+ * +----------------------------------------------------------------------
+ * | common
+ * +----------------------------------------------------------------------
+ * | Author: 1009239228@qq.com
+ * +----------------------------------------------------------------------
+ */
+import React,{Component} from 'react';
+
+import Header from './components/Header';
+
+class Common extends Component{
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+
+}
+export default Common;
