3beb2de05dda90669e6a674a29c76dda14629c2a
myj <2908037646@qq.com>
2019-05-14 16:42:21 +0800

    Updates

---
+++ precache-manifest.abe7a239f0bca7d447b222f5d94954af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42a8f2bf5344ed0aac65",
+    "url": "/app_zxs/static/js/runtime~main.61dcb951.js"
+  },
+  {
+    "revision": "7ea978c9d8280e319cb9",
+    "url": "/app_zxs/static/js/main.6746e05c.chunk.js"
+  },
+  {
+    "revision": "cbc3bf7da3a0a6d2a08f",
+    "url": "/app_zxs/static/js/2.0873c095.chunk.js"
+  },
+  {
+    "revision": "7ea978c9d8280e319cb9",
+    "url": "/app_zxs/static/css/main.96acdc9b.chunk.css"
+  },
+  {
+    "revision": "cbc3bf7da3a0a6d2a08f",
+    "url": "/app_zxs/static/css/2.3bfdf57e.chunk.css"
+  },
+  {
+    "revision": "12bc60f61ae2669ceb0fefd48953b1a8",
+    "url": "/app_zxs/index.html"
+  }
+];
